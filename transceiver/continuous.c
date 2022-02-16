a983cdac9e19dd7ae35049044b5d5e908250fe14
faust-is <faustov.97@bk.ru>
2022-02-16 12:25:06 +0300

    first commit

---
+++ transceiver/continuous.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+#if defined(_WIN32) && !defined(__CYGWIN__)
+#include <windows.h>
+#else
+#include <sys/select.h>
+#endif
+
+#include "err.h"
+#include <ad.h>
+
+
+/* Sleep for specified msec */
+static void
+sleep_msec(int32 ms)
+{
+#if (defined(_WIN32) && !defined(GNUWINCE)) || defined(_WIN32_WCE)
+    Sleep(ms);
+#else
+    /* ------------------- Unix ------------------ */
+    struct timeval tmo;
+
+    tmo.tv_sec = 0;
+    tmo.tv_usec = ms * 1000;
+
+    select(0, NULL, NULL, NULL, &tmo);
+#endif
+}
+
+/*
+ * Main utterance processing loop:
+ *     for (;;) {
+ *        start utterance and wait for speech to process
+ *        decoding till end-of-utterance silence will be detected
+ *        print utterance result;
+ *     }
+ */
+
+static void
+recognize_from_microphone()
+{
+    ad_rec_t *ad;
+    int16 adbuf[2048];
+    int32 k;
+    char const *hyp;
+
+    if ((ad = ad_open_dev(cmd_ln_str_r(config, "-adcdev"),
+                          (int) cmd_ln_float32_r(config,
+                                                 "-samprate"))) == NULL)
+        E_FATAL("Failed to open audio device\n");
+    if (ad_start_rec(ad) < 0)
+        E_FATAL("Failed to start recording\n");
+
+    if (ps_start_utt(ps) < 0)
+        E_FATAL("Failed to start utterance\n");
+
+    E_INFO("Ready....\n");
+
+    for (;;) {
+        if ((k = ad_read(ad, adbuf, 2048)) < 0)
+            E_FATAL("Failed to read audio\n");
+        
+        }
+        sleep_msec(100);
+    }
+    ad_close(ad);
+}
+
+
+int
+main(int argc, char *argv[])
+{
+    E_INFO("%s COMPILED ON: %s, AT: %s\n\n", argv[0], __DATE__, __TIME__);
+
+    recognize_from_microphone();
+    
+    return 0;
+}
+
+#if defined(_WIN32_WCE)
+#pragma comment(linker,"/entry:mainWCRTStartup")
+#include <windows.h>
+//Windows Mobile has the Unicode main only
+int
+wmain(int32 argc, wchar_t * wargv[])
+{
+    char **argv;
+    size_t wlen;
+    size_t len;
+    int i;
+
+    argv = malloc(argc * sizeof(char *));
+    for (i = 0; i < argc; i++) {
+        wlen = lstrlenW(wargv[i]);
+        len = wcstombs(NULL, wargv[i], wlen);
+        argv[i] = malloc(len + 1);
+        wcstombs(argv[i], wargv[i], wlen);
+    }
+
+    //assuming ASCII parameters
+    return main(argc, argv);
+}
+#endif
